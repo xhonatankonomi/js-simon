d1c246cf7365874d89d371463217332bacd751ea
xhonatankonomi <xhoni8962@gmail.com>
2025-10-19 15:09:36 +0200

    Refactor number generation logic in generaNumeriCasuali function

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,5 +2,18 @@ let numeriDaIndovinare = [];
 let timerId;
 
 function generaNumeriCasuali() {
-    
+    numeriDaIndovinare = [];
+    for (let contatore = 0; contatore < 100 && numeriDaIndovinare.length < 5; contatore++) {
+        let numero = Math.floor(Math.random() * 100) + 1;
+        let trovato = false;
+        for (let i = 0; i < numeriDaIndovinare.length; i++) {
+            if (numeriDaIndovinare[i] === numero) {
+                trovato = true;
+            }
+        }
+
+        if (trovato === false) {
+            numeriDaIndovinare.push(numero);
+        }
+    }
 }
